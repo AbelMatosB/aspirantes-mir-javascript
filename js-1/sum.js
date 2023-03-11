3034bf6d99146cf27a79372dbc37a7165dd3fd69
Abel <abelmatos.b@gmail.com>
2023-03-10 19:16:49 -0500

    solucion a los ejercicios de js

---
+++ js-1/sum.js
@@ -0,0 +1,12 @@
+function sum(numero) {
+    var tot=0;
+    for (var i=1;i<=numero;i++) {
+        tot+=i
+    }
+    return tot
+}
+
+// código de prueba
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
